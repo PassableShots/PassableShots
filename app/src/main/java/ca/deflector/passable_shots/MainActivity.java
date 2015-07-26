123eba9c4a4f18e66b18c259ed3a7f617bd3ed22
pdmholden <pdmholden@gmail.com>
2015-07-25 21:19:50 -0400

    First working version

--- app/src/main/java/ca/deflector/passable_shots/MainActivity.java
+++ app/src/main/java/ca/deflector/passable_shots/MainActivity.java
@@ -1,12 +1,16 @@
 package ca.deflector.passable_shots;
 
+import android.content.pm.ActivityInfo;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends Activity implements SurfaceHolder.Callback {
@@ -24,31 +28,31 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        try {
+            EncryptionSystem.initKey();
+        } catch (Exception e) {
+            //TODO show an error message and disable the button
+            Log.wtf("ps", "?", e);
+        }
+
         encryptCallback = new Camera.PictureCallback() {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
                 // call encryption here
+                try {
+                    byte[] encrypted = EncryptionSystem.encrypt(data);
+                    FileOutputStream fos = new FileOutputStream(Environment.getExternalStorageDirectory() + "/passable." + System.currentTimeMillis());
+                    fos.write(encrypted);
+                    fos.close();
+                } catch (Exception e) {
+                    Log.wtf("init", e);
+                }
+                refreshCamera();
             }
         };
-        
-        /* try {
-            File sample = new File(Environment.getExternalStorageDirectory() + "/sample.jpg");
-            byte[] bytes = new byte[(int) sample.length()];
-            FileInputStream fis = new FileInputStream(sample);
-            int count = 0;
-            while (count < bytes.length) {
-                int read = fis.read(bytes, count, bytes.length - count);
-                count += read;
-            }
-            fis.close();
-            EncryptionSystem.initKey();
-            byte[] encrypted = EncryptionSystem.encrypt(bytes);
-            FileOutputStream fos = new FileOutputStream(Environment.getExternalStorageDirectory() + "/sample.encrypted");
-            fos.write(encrypted);
-            fos.close();
-        } catch (Exception e) {
-            Log.wtf("ps", "?", e);
-        } */
+
     }
 
     public void captureImage(View v) throws IOException {
@@ -58,16 +62,53 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera = Camera.open();
+        } catch (RuntimeException e) {
+            Log.wtf("create", e);
+            return;
+        }
+        Camera.Parameters param = camera.getParameters();
 
+        param.setPreviewSize(holder.getSurfaceFrame().width(), holder.getSurfaceFrame().height());
+        camera.setParameters(param);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (Exception e) {
+            Log.wtf("create2", e);
+        }
+    }
+
+    private void refreshCamera() {
+        if (surfaceHolder.getSurface() == null)
+            return;
+
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            Log.wtf("refresh", e);
+        }
+        Camera.Parameters param = camera.getParameters();
+        param.setPreviewSize(surfaceHolder.getSurfaceFrame().width(), surfaceHolder.getSurfaceFrame().height());
+        camera.setParameters(param);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            Log.wtf("refresh2", e);
+        }
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int fmt, int w, int h) {
-
+        refreshCamera();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-
+        camera.stopPreview();
+        camera.release();
+        camera = null;
     }
 }
